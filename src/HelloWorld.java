99e50778682e293b0ed7b323f17a48763d5b99e9
Nick Adams <nick.pappas.adams@gmail.com>
2022-11-17 13:05:10 -0600

    added age field

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        Person theBestDrummer = new Person("thomas lang", 25);
+        Person theBestDrummer = new Person("thomas lang", 25, 40);
         System.out.println(theBestDrummer.getPersonInfo());
         theBestDrummer.sayHello();
         String hello = "Hello people!";
--- src/Person.java
+++ src/Person.java
@@ -1,15 +1,18 @@
+
 public class Person {
 
     public String name;
     public int yearsPlayed;
+    public int age;
 
-    public Person(String name, int yearsPlayed) {
+    public Person(String name, int yearsPlayed, int age) {
         this.name = name;
         this.yearsPlayed = yearsPlayed;
+        this.age = age;
     }
 
     public String getPersonInfo() {
-        return String.format("Name: %s, Years Played: %s", name, yearsPlayed);
+        return String.format("Name: %s, Years Played: %s, Age: %s", name, yearsPlayed, age);
     }
 
     public void sayHello() {
