8228e0e0e422e06a725d5acc93e12648b5ca4947
Nick Adams <nick.pappas.adams@gmail.com>
2022-11-18 18:06:44 -0600

    added string stuff

---
+++ src/Java1/StringExercise.java
@@ -0,0 +1,19 @@
+package Java1;
+
+public class StringExercise {
+
+    public static void main(String[] args) {
+        String myString1 = "we dont need no education";
+        String myString2 = "we dont need no thought control";
+        System.out.println(myString1 + ", " + myString2);
+
+        String str = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(str);
+
+        String newStr = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(newStr);
+
+
+    }
+}
