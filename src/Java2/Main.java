dc4d1365e48b36eae89ad7bbab17003c4f3d7eb2
Nick Adams <nick.pappas.adams@gmail.com>
2022-11-22 13:16:13 -0600

    created basic person class

--- src/Java2/Main.java
+++ src/Java2/Main.java
@@ -3,6 +3,19 @@ package Java2;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello from java 2!");
+        // instance fields of a class(objects)
+        Person nick = new Person();
+        Person mom = new Person();
+        nick.firstName = "Nick";
+        nick.lastName = "Adams";
+
+        mom.firstName = "Terry";
+        mom.lastName = "Lev";
+
+        System.out.println(nick.sayHello());
+        System.out.println(mom.sayHello());
+
+        //static field of a class
+        System.out.println(Math.PI);
     }
 }
