e1f9a665fa2e0a66f4a43e8bc63885d15321c219
Nick Adams <nick.pappas.adams@gmail.com>
2022-11-23 15:52:58 -0600

    added 2d arrays

--- src/Java2/Main.java
+++ src/Java2/Main.java
@@ -52,6 +52,26 @@ public class Main {
         System.out.println(Arrays.toString(theBeatles));
 
         // 2d arrays
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        System.out.println(matrix[0][1]);
+        System.out.println(matrix[1][0]);
+        System.out.println(matrix[2][2]);
+
+        for(int[] row : matrix) {
+            System.out.println("+---+---+---+");
+
+            System.out.print("| ");
+            for(int n : row) {
+                System.out.print(n + " | ");
+            }
+            System.out.println();
+            System.out.println("+---+---+---+");
+        }
 
 
 
